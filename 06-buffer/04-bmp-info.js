452d5a301255edb724eaf947c75cf98772f287f6
liujingjing <949694044@qq.com>
2019-09-20 21:55:14 +0800

    new

--- 06-buffer/04-bmp-info.js
+++ 06-buffer/04-bmp-info.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
 const fs=require('fs'),
       log=console.log;
-var buf =fs.readFileSync('./nodejs-logo.bmp');
+if(process.argv.length!==3){
+  console.error('命令行参数格式 ：cmd filename');
+  process.exit(1);
+}
+try{
+  var buf =fs.readFileSync('./nodejs-logo.bmp');
+}catch(e){
+  console.error(e.message);
+  process.exit(2);
+}
 log('width:',buf.readInt32LE(0x12));
 log('height:',buf.readInt32LE(0x16));
 log('depth:',buf.readUInt16LE(0x1c));
